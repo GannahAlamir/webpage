9ab97c0cdb19085bfaca5361563da2b9ad78c274
GannahAlamir <147345258+GannahAlamir@users.noreply.github.com>
2024-07-23 21:00:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ // script.js
+
+ document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("تم إرسال رسالتك بنجاح!");
+        form.reset();
+    });
+});
